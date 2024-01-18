7282ae06d9107bb9120a69c3195967b641f6fcf9
ttv-eu c1 <ttv-eu-cohort-1@ephemerecreative.ca>
2024-01-18 16:16:24 +0000

    bump logout button into the image area

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,17 +1,22 @@
-import { Button, Container } from "@mui/material";
-import Link from "next/link"
+import { Button, Container, Stack } from "@mui/material";
+import Link from "next/link";
 import { ReactNode } from "react";
 
-export default function DashboardLayout({children}: {children: ReactNode}) {
-    return (
-        <div>
-            <Container>
-                <nav>
-                    <Link href={"/auth/logout"}><Button variant="contained">Logout</Button></Link>
-                </nav>
-            </Container>
-            {children}
-        </div>
-
-    )
+export default function DashboardLayout({ children }: { children: ReactNode }) {
+  return (
+    <>
+      <Container
+        style={{ position: "fixed", top: 0, left: 0, right: 0, paddingTop: 20 }}
+      >
+        <nav>
+          <Stack direction="row" justifyContent="flex-end">
+            <Link href={"/auth/logout"}>
+              <Button variant="contained">Logout</Button>
+            </Link>
+          </Stack>
+        </nav>
+      </Container>
+      {children}
+    </>
+  );
 }
