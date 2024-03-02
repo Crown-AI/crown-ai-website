18c6cf144ea814589d3fc82f880db2c45cbffc6f
Harrison John <harrisjohnu@gmail.com>
2024-03-02 18:22:07 +0000

    Updated bg-vid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -88,7 +88,7 @@ export default function Home() {
             e.preventDefault();
           }}
         >
-          <source src="/ai-frontier.mp4" type="video/mp4" />
+          <source src="/ai-upfront.mp4" type="video/mp4" />
         </video>
       </Stack>
       <Typography
