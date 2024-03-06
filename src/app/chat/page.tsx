768f8e446ce8e5ba7edba263fdfe3f44c0242c98
Harrison John <harrisjohnu@gmail.com>
2024-03-06 20:54:39 +0000

    Updated the chat UI

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,7 +1,10 @@
 "use client";
+
 import React, { useEffect, useState } from "react";
 import Pusher from "pusher-js";
 import { Box, Stack } from "@mui/material";
+import { useRouter } from "next/navigation";
+import "../globalicons.css";
 
 interface MessageData {
   message: string;
@@ -14,6 +17,7 @@ const pusher = new Pusher(process.env.NEXT_PUBLIC_PUSHER_KEY!, {
 export default function Chat() {
   const [messages, setMessages] = useState<string[]>([]);
   const [input, setInput] = useState<string>("");
+  const router = useRouter();
 
   useEffect(() => {
     const channel = pusher.subscribe("chat");
@@ -38,56 +42,163 @@ export default function Chat() {
           backgroundSize: "cover",
           height: "100vh",
           width: "100%",
-          display: "flex",
-          flexDirection: "column",
         }}
       >
-        <Stack
+        <nav
           style={{
-            flexGrow: 1,
-            overflowY: "auto",
-            backgroundColor: "rgba(128, 128, 128, 0.3)",
-            width: 1000,
-            marginLeft: 150,
-            padding: "20px 0",
+            display: "flex",
+            position: "relative",
+            alignItems: "center",
+            justifyContent: "center",
+            wordSpacing: 2,
+            gap: 5,
           }}
         >
-          {messages.map((message, index) => (
-            <p
-              key={index}
-              style={{
-                backgroundColor: "green",
-                display: "flex",
-                position: "relative",
-                alignItems: "center",
-                justifyContent: "center",
-                borderRadius: 50,
-                height: 50,
-              }}
-            >
-              {message}
-            </p>
-          ))}
-        </Stack>
-        <div style={{ display: "flex", alignItems: "center" }}>
-          <input
-            placeholder="Message"
-            style={{ flexGrow: 1, marginRight: 10 }}
-            value={input}
-            onChange={(e) => setInput(e.target.value)}
-          />
-          <button
-            onClick={() => {
-              fetch("/api/chat", {
-                method: "POST",
-                body: JSON.stringify({ message: input }),
-              });
-              setInput("");
+          <span
+            className="material-symbols-outlined"
+            style={{ position: "relative" }}
+          >
+            home
+          </span>
+          <p
+            style={{
+              fontFamily: "'Indie Flower', cursive",
+              color: "gray",
+              cursor: "pointer",
+              position: "relative",
+            }}
+            id="home"
+            onClick={(e) => {
+              e.preventDefault();
+              router.push("/");
             }}
           >
-            Send
-          </button>
-        </div>
+            Home
+          </p>
+          <span
+            className="material-symbols-outlined"
+            id="contacts"
+            style={{ marginLeft: 40 }}
+          >
+            contacts_product
+          </span>
+          <p
+            style={{
+              color: "gray",
+              fontFamily: "'Indie Flower', cursive",
+              cursor: "pointer",
+            }}
+            onClick={(e) => {
+              e.preventDefault();
+              router.push("/contact");
+            }}
+          >
+            Contact us
+          </p>
+          <span
+            className="material-symbols-outlined"
+            style={{ marginLeft: 30 }}
+          >
+            local_library
+          </span>
+          <p
+            style={{
+              color: "gray",
+              fontFamily: "'Indie Flower', cursive",
+              cursor: "pointer",
+            }}
+            onClick={(y) => {
+              y.preventDefault();
+              router.push("/about");
+            }}
+          >
+            About us
+          </p>
+          <span
+            className="material-symbols-outlined"
+            style={{ marginLeft: 50 }}
+          >
+            chat
+          </span>
+          <p
+            id="chats"
+            style={{
+              color: "gray",
+              fontFamily: "'Indie Flower', cursive",
+              cursor: "pointer",
+            }}
+            onClick={(c) => {
+              c.preventDefault();
+              router.push("/chat");
+            }}
+          >
+            Chats & Privacy
+          </p>
+        </nav>
+        <Stack
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            position: "relative",
+            alignItems: "baseline",
+            justifyContent: "space-between",
+            backgroundColor: "rgba(128, 128, 128, 0.3)",
+            height: 600,
+            width: 1000,
+            left: 150,
+            top: 30,
+            overflow: "scroll",
+          }}
+        >
+          <div style={{ marginTop: 450, marginLeft: 400 }}>
+            {messages.map((message, index) => (
+              <p
+                key={index}
+                style={{
+                  backgroundColor: "green",
+                  display: "flex",
+                  position: "relative",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  borderRadius: 50,
+                  height: 50,
+                }}
+              >
+                {message}
+              </p>
+            ))}
+            <input
+              placeholder="Message"
+              style={{
+                marginTop: "auto",
+                alignItems: "bottom",
+                justifyContent: "bottom",
+                borderBottom: 0,
+                backgroundColor: "grey",
+                width: 500,
+                height: 50,
+                border: 0,
+                outline: 0,
+                borderRadius: 50,
+              }}
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+            />
+            <button
+              onClick={() => {
+                fetch("/api/chat", {
+                  method: "POST",
+                  body: JSON.stringify({
+                    message: input,
+                  }),
+                });
+                setInput("");
+              }}
+            >
+              Send
+            </button>
+          </div>
+        </Stack>
       </Stack>
     </Box>
   );
