0ed8851451a87bd150a752625e96a8c55f59b3ae
Harrison John <harrisjohnu@gmail.com>
2024-03-10 20:30:57 +0000

    Changed my img to b&w for professional reasons

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -234,7 +234,7 @@ export default function About() {
             >
               <Image
                 alt="HJ"
-                src={"/xeng.jpg"}
+                src={"/bw.jpg"}
                 width={200}
                 height={200}
                 style={{ borderRadius: "100px" }}
