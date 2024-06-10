6a6ada5cb084ce27ebb0fbc2bc5b4618244456e7
Harrison John <harrisjohnu@gmail.com>
2024-06-10 23:43:31 +0000

    Revert to AI

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import { type Metadata } from "next";
 export const metadata: Metadata = {
   title: {
     template: "%s - My App",
-    default: "Sypher GLobe",
+    default: "AICulture",
   },
   description:
     "An AI agriculture website intended to help people with agricultural questions and help users chat with other users for better understanding.",
