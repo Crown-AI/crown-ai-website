dbb1cec792d79cee6c8f3d50017cf79f2ae90068
Harrison John <harrisjohnu@gmail.com>
2024-06-22 15:11:54 +0000

    Cyst

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,24 +1,8 @@
 import { NavBar } from "@/components/navbar/navbar";
 import { Box, Stack, Typography } from "@mui/material";
 import "../globalicons.css";
-import { useSession } from "next-auth/react";
 
 export default function About() {
-  const session = useSession();
-  const redemption = () => {
-    let login = document.getElementById("login") as HTMLButtonElement;
-    let logout = document.getElementById("logout") as HTMLButtonElement;
-    if (session.data?.user) {
-      console.log("Status: Logged in");
-      login.style.display = "none";
-      logout.style.display = "flex";
-    } else {
-      console.log("Status: Logged out");
-      login.style.display = "flex";
-      logout.style.display = "none";
-    }
-  };
-  window.addEventListener("mouseover", redemption);
   return (
     <Box>
       <Stack>
